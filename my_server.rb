33fedc2227c1033106c863ca7236bb33e5ba8615
Michael Carroll <carrollm2@gmail.com>
2020-01-23 01:57:47 +0000

    Automatically backed up by Learn

--- my_server.rb
+++ my_server.rb
@@ -7,8 +7,4 @@ class MyServer
     (Time.now.to_i % 2).zero? ?  ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
   end
 
-  def hello_world
-    (Time.now.to_i % 2).zero? ?  ["<em>Hello, my name is</em>"] : ["<strong>Hello, my name is</strong>"]
-  end
-
 end
